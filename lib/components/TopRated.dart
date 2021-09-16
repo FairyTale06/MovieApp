7a29f0b66f2f6259495537742a3fd0487b4744e0
DESKTOP-95E4DBA\Junyi <junyi6608@gmail.com>
2021-09-16 10:02:13 +0800

    components seperated in different files

---
+++ lib/components/TopRated.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/services/movie_service.dart';
+
+class TopRated extends StatefulWidget {
+
+  String movieID;
+
+  @override
+  _TopRatedState createState() => _TopRatedState();
+}
+
+Future<List> getTopRated() async {
+  MovieService instance = MovieService();
+
+  var result = await instance.getTopRated();
+  return result;
+}
+
+class _TopRatedState extends State<TopRated> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+        physics: ScrollPhysics(),
+        child: FutureBuilder <List>(
+          future: getTopRated(),
+          builder: (context, snapshot){
+            if(snapshot.hasData){
+              List data = snapshot.data;
+              return Column(
+                children: [
+                  Text(
+                    "Top Rated Movie",
+                    style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.white
+                    ),
+                  ),
+                  ListView.builder(
+                      physics: NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: 16,
+                      itemBuilder: (BuildContext context, int index){
+                        return Padding(
+                          padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                          child: Card(
+                            color: Color(0xff5345cc),
+                            child: Padding(
+                                padding: const EdgeInsets.all(2.0),
+                                child: InkWell(
+                                  onTap: () async{
+                                    Navigator.pushNamed(context, "/detail", arguments: {"movieID": data[index]["id"]});
+                                  },
+                                  child: ListTile(
+                                    leading: Image.network('https://image.tmdb.org/t/p/w200/${data[index]['poster_path']}', height: 100),
+                                    title: Text(
+                                      data[index]['original_title'],
+                                      style: TextStyle(
+                                          color: Colors.white
+                                      ),
+                                    ),
+                                    subtitle: Text(
+                                      "Popularity: "+(data[index]['popularity']).toString(),
+                                      style: TextStyle(
+                                          color: Colors.white
+                                      ),
+                                    ),
+                                  ),
+                                )
+                            ),
+                          ),
+                        );
+                      }
+                  ),
+                ],
+              );
+            }
+            return CircularProgressIndicator();
+          },
+        )
+    );
+  }
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:movie_app/services/movie_service.dart';
+import 'package:movie_app/components/TopRated.dart';
+import 'package:movie_app/components/Upcoming.dart';
+import 'package:movie_app/components/Popular.dart';
 
 class Home extends StatefulWidget {
 
@@ -12,212 +14,6 @@ class _HomeState extends State<Home> {
   String name;
   int _currentTabIndex = 0;
 
-  Future<List> getTopRated() async {
-    MovieService instance = MovieService();
-
-    var result = await instance.getTopRated();
-    return result;
-  }
-
-  Future<List> getUpcoming() async {
-    MovieService instance = MovieService();
-
-    var result = await instance.getUpcoming();
-    return result;
-  }
-
-  Future<List> getPopular() async {
-    MovieService instance = MovieService();
-
-    var result = await instance.getPopular();
-    return result;
-  }
-
-  Widget popular(){
-    return SingleChildScrollView(
-        physics: ScrollPhysics(),
-        child: FutureBuilder <List>(
-          future: getPopular(),
-          builder: (context, snapshot){
-            if(snapshot.hasData){
-              List data = snapshot.data;
-              return Column(
-                children: [
-                  Text(
-                    "Popular Movie",
-                    style: TextStyle(
-                        fontSize: 26,
-                        color: Colors.white
-                    ),
-                  ),
-                  ListView.builder(
-                      physics: NeverScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      itemCount: 16,
-                      itemBuilder: (BuildContext context, int index){
-                        return Padding(
-                          padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-                          child: Card(
-                            color: Color(0xff5345cc),
-                            child: Padding(
-                                padding: const EdgeInsets.all(2.0),
-                                child: InkWell(
-                                  onTap: () async{
-                                    Navigator.pushNamed(context, "/detail", arguments: {"movieID": data[index]["id"]});
-                                  },
-                                  child: ListTile(
-                                    leading: Image.network('https://image.tmdb.org/t/p/w200/${data[index]['poster_path']}', height: 100),
-                                    title: Text(
-                                      data[index]['original_title'],
-                                      style: TextStyle(
-                                          color: Colors.white
-                                      ),
-                                    ),
-                                    subtitle: Text(
-                                      "Popularity: "+(data[index]['popularity']).toString(),
-                                      style: TextStyle(
-                                          color: Colors.white
-                                      ),
-                                    ),
-                                  ),
-                                )
-                            ),
-                          ),
-                        );
-                      }
-                  ),
-                ],
-              );
-            }
-            return CircularProgressIndicator();
-          },
-        )
-    );
-  }
-
-  Widget upcoming(){
-    return SingleChildScrollView(
-        physics: ScrollPhysics(),
-        child: FutureBuilder <List>(
-          future: getUpcoming(),
-          builder: (context, snapshot){
-            if(snapshot.hasData){
-              List data = snapshot.data;
-              return Column(
-                children: [
-                  Text(
-                    "Upcoming Movie",
-                    style: TextStyle(
-                        fontSize: 26,
-                        color: Colors.white
-                    ),
-                  ),
-                  ListView.builder(
-                      physics: NeverScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      itemCount: 16,
-                      itemBuilder: (BuildContext context, int index){
-                        return Padding(
-                          padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-                          child: Card(
-                            color: Color(0xff5345cc),
-                            child: Padding(
-                                padding: const EdgeInsets.all(2.0),
-                                child: InkWell(
-                                  onTap: () async{
-                                    Navigator.pushNamed(context, "/detail", arguments: {"movieID": data[index]["id"]});
-                                  },
-                                  child: ListTile(
-                                    leading: Image.network('https://image.tmdb.org/t/p/w200/${data[index]['poster_path']}', height: 100),
-                                    title: Text(
-                                      data[index]['original_title'],
-                                      style: TextStyle(
-                                          color: Colors.white
-                                      ),
-                                    ),
-                                    subtitle: Text(
-                                      "Popularity: "+(data[index]['popularity']).toString(),
-                                      style: TextStyle(
-                                          color: Colors.white
-                                      ),
-                                    ),
-                                  ),
-                                )
-                            ),
-                          ),
-                        );
-                      }
-                  ),
-                ],
-              );
-            }
-            return CircularProgressIndicator();
-          },
-        )
-    );
-  }
-
-  Widget topRated(){
-    return SingleChildScrollView(
-      physics: ScrollPhysics(),
-      child: FutureBuilder <List>(
-        future: getTopRated(),
-        builder: (context, snapshot){
-          if(snapshot.hasData){
-            List data = snapshot.data;
-            return Column(
-              children: [
-                Text(
-                    "Top Rated Movie",
-                  style: TextStyle(
-                    fontSize: 26,
-                    color: Colors.white
-                  ),
-                ),
-                ListView.builder(
-                    physics: NeverScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                    itemCount: 16,
-                    itemBuilder: (BuildContext context, int index){
-                      return Padding(
-                        padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-                        child: Card(
-                          color: Color(0xff5345cc),
-                          child: Padding(
-                            padding: const EdgeInsets.all(2.0),
-                            child: InkWell(
-                              onTap: () async{
-                                Navigator.pushNamed(context, "/detail", arguments: {"movieID": data[index]["id"]});
-                              },
-                              child: ListTile(
-                                leading: Image.network('https://image.tmdb.org/t/p/w200/${data[index]['poster_path']}', height: 100),
-                                title: Text(
-                                  data[index]['original_title'],
-                                  style: TextStyle(
-                                      color: Colors.white
-                                  ),
-                                ),
-                                subtitle: Text(
-                                  "Popularity: "+(data[index]['popularity']).toString(),
-                                  style: TextStyle(
-                                      color: Colors.white
-                                  ),
-                                ),
-                              ),
-                            )
-                          ),
-                        ),
-                      );
-                    }
-                ),
-              ],
-            );
-          }
-          return CircularProgressIndicator();
-        },
-      )
-    );
-  }
 
 //   Widget carousel(){
 //     return Container(
@@ -242,19 +38,27 @@ class _HomeState extends State<Home> {
 //     );
 // }
 
+  TopRated topRated = TopRated();
+  Upcoming upcoming = Upcoming();
+  Popular popular = Popular();
+
   @override
   Widget build(BuildContext context) {
 
     final _kTabPages = <Widget>[
-      Center(child: topRated()),
-      Center(child: popular()),
-      Center(child: upcoming()),
+      Center(child: topRated),
+      Center(child: popular),
+      Center(child: upcoming),
+      Center(child: popular),
+      Center(child: popular),
     ];
 
     final _kBottomNavBarItems = <BottomNavigationBarItem>[
       const BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Top Rated'),
       const BottomNavigationBarItem(icon: Icon(Icons.local_fire_department), label: 'Popular'),
+      const BottomNavigationBarItem(icon: Icon(Icons.av_timer), label: 'HOME'),
       const BottomNavigationBarItem(icon: Icon(Icons.av_timer), label: 'Upcoming'),
+      const BottomNavigationBarItem(icon: Icon(Icons.av_timer), label: 'Search'),
     ];
 
     assert(_kTabPages.length == _kBottomNavBarItems.length);
